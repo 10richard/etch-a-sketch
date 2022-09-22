3efe2f8c816d7b115c66f3fcfdbb7ca7e6831451
Richard <rten04@icloud.com>
2022-09-22 00:25:22 -0400

    Add functions
    Added needed functions that will be used for future features such as setting
    the marker as a "rainbow" and being able to "clear" the board.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
 const body = document.querySelector('body');
+const defaultBlack = document.querySelector('#default-black');
+const rainbow = document.querySelector('#rainbow');
+const clear = document.querySelector('#clear');
+const gridContainer = document.querySelector('.grid-container');
+
 const grid = document.createElement('div');
 grid.setAttribute('id', 'grid');
-body.appendChild(grid);
+gridContainer.appendChild(grid);
 
+//Creates a 16x16 grid
 function createDefaultGrid() {
     for (let i = 0; i < 256; i++) {
         const gridSquare = document.createElement('div');
@@ -12,6 +18,25 @@ function createDefaultGrid() {
     }
 }
 
-function
+function checkHover(gridItem) {
+    gridItem.addEventListener('click', () => {
+        gridItem.classList.add = ("black");
+    });
+}
+
+function changeRainbow() {
+    
+}
+
+function clearGrid() {
+
+}
+
+function changeGrid() {
+
+}
+
+rainbow.addEventListener('click', changeRainbow);
+clear.addEventListener('click', clearGrid);
 
 createDefaultGrid()
