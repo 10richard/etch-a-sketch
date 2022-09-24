c4c66fe74b32a4d96ae9677369ae886b684ab95f
Richard <rten04@icloud.com>
2022-09-24 00:03:57 -0400

    Restart from scratch/set grid size
    Completely wiped file and starting anew. Added a function that creates a
    grid on the webpage. (will incorporate with a slider in the future)

--- script.js
+++ script.js
@@ -1,42 +1,20 @@
+let size = 16; //Default grid size 16
 
-const body = document.querySelector('body');
-const defaultBlack = document.querySelector('#default-black');
-const rainbow = document.querySelector('#rainbow');
-const clear = document.querySelector('#clear');
+
 const gridContainer = document.querySelector('.grid-container');
 
 const grid = document.createElement('div');
 grid.setAttribute('id', 'grid');
 gridContainer.appendChild(grid);
 
-//Creates a 16x16 grid
-function createDefaultGrid() {
-    for (let i = 0; i < 256; i++) {
+
+function setGridSize(scale) {
+    for (let i = 0; i < scale ** 2; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-item');
         grid.appendChild(gridSquare);
-        gridSquare.addEventListener('mouseover', () => {
-            gridSquare.classList.add('black');
-        });
-    }
+    };
+    grid.style.cssText = `grid-template-columns: repeat(${scale}, 1fr)`;
 }
 
-function changeRainbow() {
-    
-}
-
-function clearGrid() {
-    const gridSquares = document.querySelectorAll('.grid-item');
-    gridSquares.forEach(gridSquare => {
-        gridSquare.classList.remove('black', 'rainbow');
-    });
-}
-
-function changeGrid() {
-
-}
-
-rainbow.addEventListener('click', changeRainbow);
-clear.addEventListener('click', clearGrid);
-
-createDefaultGrid()
+setGridSize(32);
