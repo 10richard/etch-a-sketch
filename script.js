c142abd7776ac8ba51e101f074f964b6934f66db
Richard <rten04@icloud.com>
2022-09-25 17:50:21 -0400

    Finish product
    Grid Information
    Set default grid size to 16x16.
    
    Added .addEventListener('click') to changeGridBtn. When user inputs val
    and clicks resize button, checkGridResize function will be called. Inside
    the function, the value input is checked to make sure it is between 2 and
    100. If not, gridMessage writes an error on webpage. If yes, setGrid function
    is called. Inside setGrid function, all gridItems(divs) are removed from
    grid container. Then, the value specified is squared and looped through.
    Throughout each iteration, a div element is appended to gridContainer and
    an event lister for 'mouseover' is added to each one. In addition, the style
    "grid-template-columns:" on gridContainer is set to "repeat('specified value', 1fr)"
    
    Draw on grid
    Added let variable 'click' and set to false. Added event listener 'click' to gridContainer.
    If user clicks on anything inside of gridContainer, click will be set to
    the opposite of what it currently is. (if currently true, then set to false
    and vice verse) Going back to setGrid function inside of the loop, we added
    event listener 'mouseover' to each gridSquare. This means if mouse is hovering
    over any of the grid squares then the colorSquare function will be called. Inside colorSquare function,
    if 'click' variable is true, then the color (black or rainbow) is chosen and coloring mode will be shown as
    "on" on the webpage/turned to green. If 'click' is false, then coloring mode will be shown as
    "off" on the webpage/turned to red.
    
    Change color
    Added let variable 'black' and set to true. Also added rainbowChoices, which has an array of
    roy g. biv strings. Added event listener 'click' to blackColor and rainbowColor. If rainbowColor
    is triggered, black is changed to false. If blackColor is triggered, black is changed to true.
    
    Resetting grid
    Added event listener 'click' to clearGrid which is linked up to a button on the
    webpage. If user clicks, then resetGrid function is called. Inside the function,
    all grid squares inside of the grid container have their background color set
    back to white.

--- script.js
+++ script.js
@@ -1,20 +1,80 @@
-let size = 16; //Default grid size 16
+let click = false;
+let black = true;
+const rainbowChoices = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
+const gridContainer = document.querySelector('.gridContainer');
+const changeGrid = document.getElementById('changeGrid');
+const blackColor = document.getElementById('black');
+const rainbowColor = document.getElementById('rainbow');
+const clearGrid = document.getElementById('clear');
+const changeGridBtn = document.querySelector('.changeGridBtn');
+let inputGridVal = document.getElementById('inputGridVal');
+let currentGridSize = document.querySelector('.currentGridSize');
+let gridMessage = document.querySelector('.gridMessage');
 
-const gridContainer = document.querySelector('.grid-container');
+let colorMode = document.querySelector('.mode');
 
-const grid = document.createElement('div');
-grid.setAttribute('id', 'grid');
-gridContainer.appendChild(grid);
-
-
-function setGridSize(scale) {
-    for (let i = 0; i < scale ** 2; i++) {
-        const gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-item');
-        grid.appendChild(gridSquare);
-    };
-    grid.style.cssText = `grid-template-columns: repeat(${scale}, 1fr)`;
+function checkGrideResize() {
+    let gridVal = inputGridVal.value;
+    if (gridVal <= 1 || gridVal > 100) {
+        gridMessage.textContent = `Cannot resize grid to ${gridVal}x${gridVal}`;
+    } else {
+        currentGridSize.textContent = `Current Size: ${gridVal}x${gridVal}`;
+        gridMessage.textContent = `Resized grid to ${gridVal}x${gridVal}`;
+        setGrid(gridVal)
+    }
 }
 
-setGridSize(32);
+function setGrid(scale) {
+    const gridSquares = document.querySelectorAll('.gridItem');
+    gridSquares.forEach(gridSquare => {gridContainer.removeChild(gridSquare)});
+    for (let i = 0; i < scale ** 2; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridItem');
+        gridSquare.addEventListener('mouseover', colorSquare);
+        gridContainer.style.cssText = `grid-template-columns: repeat(${scale}, 1fr)`;
+        gridContainer.appendChild(gridSquare);
+    }
+}
+
+setGrid(16);
+
+function colorSquare() {
+    if (click) {
+        colorMode.textContent = 'Color Mode: On';
+        colorMode.classList.add('active');
+        if (black) {
+            this.style.cssText = 'background-color: black';
+        } else {
+            let randomNum = Math.floor(Math.random(0) * 6);
+            let rainbow = rainbowChoices[randomNum];
+            this.style.cssText = `background-color: ${rainbow}`
+        }
+    } else {
+        colorMode.textContent = 'Color Mode: Off';
+        colorMode.classList.remove('active');
+    }
+}
+
+function resetGrid() {
+    const gridSquares = document.querySelectorAll('.gridItem');
+    gridSquares.forEach(gridSquare => {
+        gridSquare.style.cssText = 'background-color: white';
+    })
+}
+
+changeGridBtn.addEventListener('click', checkGrideResize);
+
+gridContainer.addEventListener('click', () => {
+    click = !click;
+});
+
+clearGrid.addEventListener('click', resetGrid);
+
+rainbowColor.addEventListener('click', () => {
+    black = false;
+});
+
+blackColor.addEventListener('click', () => {
+    black = true;
+});
